6f67a3e1f1aea87a5e552e5a84b004180bd4c1e0
phrobinet <ph.robinet@gmail.com>
2023-10-21 17:12:47 +0200

    add nuxt-content

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'French Nuxt',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
@@ -21,7 +21,7 @@ export default defineAppConfig({
     },
     aside: {
       level: 0,
-      collapsed: false,
+      collapsed: true,
       exclude: []
     },
     main: {
